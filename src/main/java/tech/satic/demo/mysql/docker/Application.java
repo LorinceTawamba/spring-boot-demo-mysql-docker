10f0c6b327bb0f51329623367ff77173c20ad278
LorinceTawamba <lorince.tawamba@gmail.com>
2022-02-13 20:21:08 +0100

    Update Application.java (start config)

--- src/main/java/tech/satic/demo/mysql/docker/Application.java
+++ src/main/java/tech/satic/demo/mysql/docker/Application.java
@@ -1,13 +1,40 @@
+// Package
 package tech.satic.demo.mysql.docker;
 
+// Librairie
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * Classe principale de lancement de l'application.
+ * @version     1.0.0
+ * @since       13/02/2022
+ * @author      TAWAMBA KOUAKAM Lorince S.
+ */
 @SpringBootApplication
 public class Application {
 
+    /**
+     * Methode principale de lancement de l'application.
+     * @param       args tableau des arguments fournis sur la ligne de commande.
+     * @author      TAWAMBA KOUAKAM Lorince S.
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * Methode d'exécution des traitements du microservice.
+     * @return       CommandLineRunner : traitement à exécuter.
+     * @author      TAWAMBA KOUAKAM Lorince S.
+     */
+    @Bean
+    CommandLineRunner start() {
+        return args -> {
+            System.out.println("==== SPRING BOOT DEMO MYSQL DOCKER ====");
+        };
     }
 
 }
