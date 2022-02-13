b3f9cb3448bf603342bd380e79e75d6d850cd422
LorinceTawamba <lorince.tawamba@gmail.com>
2022-02-13 20:15:59 +0100

    Update config pom.xml

--- src/main/java/tech/satic/demo/mysql/docker/Application.java
+++ src/main/java/tech/satic/demo/mysql/docker/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemoMysqlDockerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoMysqlDockerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
