9235e019ebd623b85ddae811900a5471b2d027a0
LorinceTawamba <lorince.tawamba@gmail.com>
2022-02-13 19:23:48 +0100

    Initial commit

---
+++ src/main/java/tech/satic/demo/mysql/docker/SpringBootDemoMysqlDockerApplication.java
@@ -0,0 +1,13 @@
+package tech.satic.demo.mysql.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoMysqlDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoMysqlDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/satic/demo/mysql/docker/SpringBootDemoMysqlDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.satic.demo.mysql.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoMysqlDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
